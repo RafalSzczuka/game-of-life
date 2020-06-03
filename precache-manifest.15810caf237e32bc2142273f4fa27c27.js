d5ba053ade830dd50e49816582756a7b06268f9e
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-06-03 15:29:52 +0200

    Updates

---
+++ precache-manifest.15810caf237e32bc2142273f4fa27c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60242f1a7174df607cc6d2b8a023a31",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "fd058ca941f195121329",
+    "url": "/game-of-life/static/css/main.7ca745b1.chunk.css"
+  },
+  {
+    "revision": "c9caf5647fcbe5359f0e",
+    "url": "/game-of-life/static/js/2.609babae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.609babae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd058ca941f195121329",
+    "url": "/game-of-life/static/js/main.85284e1b.chunk.js"
+  },
+  {
+    "revision": "74c0ec62b28c760df103",
+    "url": "/game-of-life/static/js/runtime-main.ccc8ab68.js"
+  }
+]);
